81f1849ae6d8e45dc3a680b081950d709eb03d1f
Darren McGarry <darren.mcgarry@hotmail.com>
2017-10-20 21:51:12 +0100

    Darren: added blank backend project with initial configuration and folder structure

---
+++ CSC3045-CS2-Backend/src/main/java/uk/ac/qub/csc3045cs2/csc3045cs2/Csc3045Cs2Application.java
@@ -0,0 +1,12 @@
+package uk.ac.qub.csc3045cs2.csc3045cs2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Csc3045Cs2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Csc3045Cs2Application.class, args);
+	}
+}
