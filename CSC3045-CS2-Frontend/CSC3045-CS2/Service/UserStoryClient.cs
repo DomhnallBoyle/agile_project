a03724ca5348668a7d526751997afc13e038d9b7
Kevin Martin <kmartin40@qub.ac.uk>
2017-10-31 11:01:54 +0000

    Kevin: Creating UI for User Story Creation, Needs Updated when Project Creation exists

---
+++ CSC3045-CS2-Frontend/CSC3045-CS2/Service/UserStoryClient.cs
@@ -0,0 +1,31 @@
+﻿using CSC3045_CS2.Models;
+using CSC3045_CS2.Utility;
+using RestSharp;
+
+namespace CSC3045_CS2.Service
+{
+    class UserStoryClient : ServiceClient
+    {
+        const string BASE_ENDPOINT = "story/";
+
+        public UserStoryClient() : base() { }
+
+        /// <summary>
+        /// Sends the User Story to be created to the backend
+        /// </summary>
+        /// <param name="userStory">The User Story to be Created</param>
+        /// <returns>The created User Story if successful, or will throw RestResponseException if error</returns>
+        public UserStory CreateUserStory(UserStory userStory)
+        {
+            var request = new RestRequest(BASE_ENDPOINT + "create", Method.POST);
+            request.AddHeader("Content-Type", "application/json");
+            request.RequestFormat = DataFormat.Json;
+            SimpleJson.CurrentJsonSerializerStrategy = new CamelCaseSerializationStrategy();
+
+            request.AddBody(userStory);
+
+            return Execute<UserStory>(request);
+        }
+
+    }
+}
