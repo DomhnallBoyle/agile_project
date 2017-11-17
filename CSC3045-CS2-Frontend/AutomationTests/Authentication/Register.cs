708fc8985b0754840d3904dfd244e1acb2ddd67d
Darren Moura McGarry <40126915@eeecs.qub.ac.uk>
2017-11-17 12:05:30 +0000

    More additions

---
+++ CSC3045-CS2-Frontend/AutomationTests/Authentication/Register.cs
@@ -0,0 +1,25 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TestStack.White.UIItems;
+
+namespace AutomationTests.Authentication
+{
+    [TestFixture]
+    public class Register : BaseTestClass
+    {
+        [Test]
+        public void RegisterValidCredentials()
+        {
+            Button registerButton = MainWindow.Get<Button>("LoginButton");
+            registerButton.Click();
+
+            Label registerPageTitle = MainWindow.Get<Label>("PageTitle");
+
+            Assert.That(registerPageTitle.Text, Is.EqualTo("Registration"));
+        }
+    }
+}
---
+++ CSC3045-CS2-Frontend/AutomationTests/BaseTestClass.cs
@@ -0,0 +1,27 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TestStack.White;
+using TestStack.White.Factory;
+using TestStack.White.UIItems.WindowItems;
+
+namespace AutomationTests
+{
+    public abstract class BaseTestClass
+    {
+        private TestContext _context;
+
+        public Window MainWindow { get; private set; }
+
+        protected BaseTestClass()
+        {
+            var appPath = Path.Combine(_context.TestDirectory, "csc3045-cs2.exe");
+            Application application = Application.Launch(appPath);
+            MainWindow = application.GetWindow("MainWindow", InitializeOption.NoCache);
+        }
+    }
+}
