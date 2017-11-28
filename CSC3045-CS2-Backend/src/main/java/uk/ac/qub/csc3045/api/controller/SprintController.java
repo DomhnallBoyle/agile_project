b5075fe1e4fe1bf7631743f3f023025ee78dcbc4
Conor Murray <cmurray87@qub.ac.uk>
2017-11-28 21:42:35 +0000

    Conor: Sprint endpoint changes

--- CSC3045-CS2-Backend/src/main/java/uk/ac/qub/csc3045/api/controller/SprintController.java
+++ CSC3045-CS2-Backend/src/main/java/uk/ac/qub/csc3045/api/controller/SprintController.java
@@ -4,6 +4,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import uk.ac.qub.csc3045.api.exception.ResponseErrorException;
 import uk.ac.qub.csc3045.api.model.Sprint;
 import uk.ac.qub.csc3045.api.model.User;
 import uk.ac.qub.csc3045.api.service.SprintService;
@@ -12,7 +14,7 @@ import javax.validation.Valid;
 import java.util.List;
 
 @RestController
-@RequestMapping(value = "/sprint")
+@RequestMapping(value = "/project/{projectId}/sprint")
 public class SprintController {
 
 	private final SprintService sprintService;
@@ -23,32 +25,36 @@ public class SprintController {
     }
 		
     @PostMapping()
-    public ResponseEntity<Sprint> createSprint(@Valid @RequestBody Sprint sprint) {
-        return new ResponseEntity<>(this.sprintService.createSprint(sprint), HttpStatus.CREATED);
+    public ResponseEntity<Sprint> createSprint(@PathVariable("projectId") long projectId, @Valid @RequestBody Sprint sprint) {
+        return new ResponseEntity<>(this.sprintService.createSprint(projectId, sprint), HttpStatus.CREATED);
     }
 
-    @GetMapping(value = "/project/{projectId}")
+    @GetMapping()
     public ResponseEntity<List<Sprint>> getProjectSprints(@PathVariable("projectId") long projectId) {
         return new ResponseEntity<>(this.sprintService.getProjectSprints(projectId), HttpStatus.OK);
     }
     
     @GetMapping(value = "/{sprintId}")
-    public ResponseEntity<Sprint> getSprint(@PathVariable("sprintId") long sprintId) {
-        return new ResponseEntity<>(sprintService.getSprint(sprintId), HttpStatus.OK);
+    public ResponseEntity<Sprint> getSprint(@PathVariable("projectId") long projectId, @PathVariable("sprintId") long sprintId) {
+        return new ResponseEntity<>(sprintService.getSprint(projectId, sprintId), HttpStatus.OK);
     }
 
-    @GetMapping(value = "/team/{sprintId}")
-    public ResponseEntity<List<User>> getSprintTeam(@PathVariable("sprintId") long sprintId) {
-        return new ResponseEntity<>(this.sprintService.getSprintTeam(sprintId), HttpStatus.OK);
+    @GetMapping(value = "/{sprintId}/user")
+    public ResponseEntity<List<User>> getSprintTeam(@PathVariable("projectId") long projectId, @PathVariable("sprintId") long sprintId) {
+        return new ResponseEntity<>(this.sprintService.getSprintTeam(projectId, sprintId), HttpStatus.OK);
     }
 	
-	@PutMapping(value = "/team")
-    public ResponseEntity<List<User>> updateSprintTeam(@Valid @RequestBody Sprint sprint) {
-        return new ResponseEntity<>(this.sprintService.updateSprintTeam(sprint), HttpStatus.OK);
+	@PutMapping(value = "/{sprintId}/user")
+    public ResponseEntity<List<User>> updateSprintTeam(@PathVariable("projectId") long projectId,@PathVariable("sprintId") long sprintId, @Valid @RequestBody Sprint sprint) {
+		if(sprint.getId() != null && (sprint.getId() != sprintId)) {
+			throw new ResponseErrorException("Id in URL and body to not match", HttpStatus.BAD_REQUEST);
+		}
+		sprint.setId(sprintId);
+        return new ResponseEntity<>(this.sprintService.updateSprintTeam(projectId, sprint), HttpStatus.OK);
     }
 
-    @GetMapping(value = "/available/team/{sprintId}")
-    public ResponseEntity<List<User>> getAvailableDevelopers(@PathVariable("sprintId") long sprintId) {
-        return new ResponseEntity<>(this.sprintService.getAvailableDevelopers(sprintId), HttpStatus.OK);
+    @GetMapping(value = "{sprintId}/user/available")
+    public ResponseEntity<List<User>> getAvailableDevelopers(@PathVariable("projectId") long projectId, @PathVariable("sprintId") long sprintId) {
+        return new ResponseEntity<>(this.sprintService.getAvailableDevelopers(projectId, sprintId), HttpStatus.OK);
     }
 }
