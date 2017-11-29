cda568f2edcfc181036d746046c65f64b7842af6
Zoey Longridge <zlongridge01@qub.ac.uk>
2017-11-29 22:22:15 +0000

    Zoey:E2E testing start

---
+++ CSC3045-CS2-Frontend/AutomationTests/Tests/UserStory/CreateUserStory.cs
@@ -0,0 +1,49 @@
+﻿using AutomationTests.PageTemplates;
+using AutomationTests.Util;
+using NUnit.Framework;
+
+namespace AutomationTests.Tests.UserStory
+{
+    [TestFixture]
+    class CreateUserStory : BaseTestClass
+    {
+        private UserDashboardPage _userDashboardPage;
+
+        private ProjectDashboardPage _projectDashboardPage;
+
+        [OneTimeSetUp]
+        public void OneTimeSetupLogin()
+        {
+            _userDashboardPage = new UserDashboardPage(MainWindow);
+        }
+
+        [Test]
+        public void ShouldSuccessfullyCreateAUserStoryAsAProductOwner()
+        {
+            LoginPage.Login("user1@e2e.com", "Aut0mation");
+
+            var messageBox = MessageBoxUtil.GetInfoMessageBox(MainWindow);
+            Assert.NotNull(messageBox);
+
+            MessageBoxUtil.ClickOKButton(messageBox);
+
+            Assert.IsTrue(_userDashboardPage.IsCurrentPage());
+
+            //Click Project
+
+            Assert.IsTrue(_projectDashboardPage.IsCurrentPage());
+
+            _projectDashboardPage.ProductBacklogButton.OnClick();
+            _userDashboardPage.
+            _projectDashboardPage.
+
+        }
+
+        [Test]
+        public void ShouldFailCreateAUserStoryNotAsAProductOwner()
+        {
+
+        }
+
+    }
+}
