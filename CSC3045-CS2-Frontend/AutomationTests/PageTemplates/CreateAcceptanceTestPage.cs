a2d5e81ef44039f096f3c106ce9fba78588b2213
Zoey Longridge <zlongridge01@qub.ac.uk>
2017-11-30 09:45:35 +0000

    Zoey:Added Acceptance Test Pages

---
+++ CSC3045-CS2-Frontend/AutomationTests/PageTemplates/CreateAcceptanceTestPage.cs
@@ -0,0 +1,73 @@
+﻿using TestStack.White.UIItems;
+using TestStack.White.UIItems.WindowItems;
+
+namespace AutomationTests.PageTemplates
+{
+    public class CreateAcceptanceTestPage : BasePage
+    {
+    
+        public override bool IsCurrentPage()
+        {
+            return PageTitle.Text.Equals("");
+        }
+
+        public Button CreateButton
+        {
+            get { return MainWindow.Get<Button>("CreateButton"); }
+        }
+
+        public Button CancelButton
+        {
+            get { return MainWindow.Get<Button>("CancelButton"); }
+        }
+
+        public TextBox GivenTextBox
+        {
+            get { return MainWindow.Get<TextBox>("GivenTextBox"); }
+        }
+
+        public TextBox WhenTextBox
+        {
+            get { return MainWindow.Get<TextBox>("WhenTextBox"); }
+        }
+
+        public TextBox ThenTextBox
+        {
+            get { return MainWindow.Get<TextBox>("ThenTextBox"); }
+        }
+
+        public void enterCorrectAcceptanceDetails(string given, string when, string then)
+        {
+            GivenTextBox.Text = given;
+            whenTextBox.Text = when;
+            thenTextBox.Text = then;
+
+            CreateButton.Click();
+        }
+
+        public void enterEmptyGivenAcceptanceDetails(string when, string then)
+        {
+            whenTextBox.Text = when;
+            thenTextBox.Text = then;
+
+            CreateButton.Click();
+        }
+
+        public void enterEmptyWhenAcceptanceDetails(string given, string then)
+        {
+            GivenTextBox.Text = given;
+            thenTextBox.Text = then;
+
+            CreateButton.Click();
+        }
+
+        public void enterEmptyThenAcceptanceDetails(string given, string when)
+        {
+            GivenTextBox.Text = given;
+            whenTextBox.Text = when;
+
+            CreateButton.Click();
+        }
+    }
+
+}
---
+++ CSC3045-CS2-Frontend/AutomationTests/Tests/AcceptanceTest/CreateAcceptanceTest.cs
@@ -0,0 +1,186 @@
+﻿using AutomationTests.PageTemplates;
+using AutomationTests.Util;
+using NUnit.Framework;
+using TestStack.White.UIItems.ListBoxItems;
+
+namespace AutomationTests.Tests.AcceptanceTest
+{
+    class CreateAcceptanceTest : BaseTestClass
+    {
+        private UserDashboardPage _userDashboardPage;
+
+        private ProjectDashboardPage _projectDashboardPage;
+
+        private ProductBacklogPage _productBacklogPage;
+
+        private CreateUserStoryPage _createUserStoryPage;
+
+        [OneTimeSetUp]
+        public void OneTimeSetupLogin()
+        {
+            _userDashboardPage = new UserDashboardPage(MainWindow);
+            _projectDashboardPage = new ProjectDashboardPage(MainWindow);
+            _productBacklogPage = new ProductBacklogPage(MainWindow);
+           
+        }
+
+        [Test]
+        public void ShouldSuccessfullyCreateAUserStoryAsAProductOwner()
+        {
+            LoginPage.Login("user2@e2e.com", "Aut0mation");
+            Assert.IsTrue(_userDashboardPage.IsCurrentPage());
+
+            var projectListItem = (WPFListItem)_userDashboardPage.ProjectListBox.Items.Find(item => "e2eProjectName1".Equals(item.Text));
+            projectListItem.Click();
+
+            Assert.IsTrue(_projectDashboardPage.IsCurrentPage());
+
+            Assert.AreEqual("e2eForename2 e2eSurname2", _projectDashboardPage.ProjectManagerNameTextBlock.Text);
+
+            _projectDashboardPage.ProductBacklogButton.Click();
+
+            Assert.IsTrue(_productBacklogPage.IsCurrentPage());
+
+            _productBacklogPage.CreateStoryButton.Click();
+
+            _createUserStoryPage.enterCorrectStoryDetails("e2eUserStory1", "10", "e2eDescription");
+
+            Assert.IsTrue(_productBacklogPage.IsCurrentPage());
+
+            var StoryListItem = (WPFListItem)_productBacklogPage.StoryListBox.Items.Find(item => "e2eUserStory1".Equals(item.Text));
+        }
+
+        [Test]
+        public void ShouldSuccessfullyAccessCreateAUserStoryAndCancel()
+        {
+            LoginPage.Login("user2@e2e.com", "Aut0mation");
+            Assert.IsTrue(_userDashboardPage.IsCurrentPage());
+
+            var projectListItem = (WPFListItem)_userDashboardPage.ProjectListBox.Items.Find(item => "e2eProjectName1".Equals(item.Text));
+            projectListItem.Click();
+
+            Assert.IsTrue(_projectDashboardPage.IsCurrentPage());
+
+            Assert.AreEqual("e2eForename2 e2eSurname2", _projectDashboardPage.ProjectManagerNameTextBlock.Text);
+
+            _projectDashboardPage.ProductBacklogButton.Click();
+
+            Assert.IsTrue(_productBacklogPage.IsCurrentPage());
+
+            _productBacklogPage.CreateStoryButton.Click();
+
+            _createUserStoryPage.CancelButton.Click();
+
+            Assert.IsTrue(_productBacklogPage.IsCurrentPage());
+        }
+
+        [Test]
+        public void ShouldFailCreatingAUserStoryWithEmptyName()
+        {
+            LoginPage.Login("user2@e2e.com", "Aut0mation");
+            Assert.IsTrue(_userDashboardPage.IsCurrentPage());
+
+            var projectListItem = (WPFListItem)_userDashboardPage.ProjectListBox.Items.Find(item => "e2eProjectName1".Equals(item.Text));
+            projectListItem.Click();
+
+            Assert.IsTrue(_projectDashboardPage.IsCurrentPage());
+
+            Assert.AreEqual("e2eForename2 e2eSurname2", _projectDashboardPage.ProjectManagerNameTextBlock.Text);
+
+            _projectDashboardPage.ProductBacklogButton.Click();
+
+            Assert.IsTrue(_productBacklogPage.IsCurrentPage());
+
+            _productBacklogPage.CreateStoryButton.Click();
+
+            _createUserStoryPage.enterEmptyNameStoryDetails("10", "e2eDescription");
+
+            var messageBox = MessageBoxUtil.GetWarningMessageBox(MainWindow);
+            Assert.NotNull(messageBox);
+
+            MessageBoxUtil.ClickOKButton(messageBox);
+        }
+
+        [Test]
+        public void ShouldFailCreatingAUserStoryWithEmptyDescription()
+        {
+            LoginPage.Login("user2@e2e.com", "Aut0mation");
+            Assert.IsTrue(_userDashboardPage.IsCurrentPage());
+
+            var projectListItem = (WPFListItem)_userDashboardPage.ProjectListBox.Items.Find(item => "e2eProjectName1".Equals(item.Text));
+            projectListItem.Click();
+
+            Assert.IsTrue(_projectDashboardPage.IsCurrentPage());
+
+            Assert.AreEqual("e2eForename2 e2eSurname2", _projectDashboardPage.ProjectManagerNameTextBlock.Text);
+
+            _projectDashboardPage.ProductBacklogButton.Click();
+
+            Assert.IsTrue(_productBacklogPage.IsCurrentPage());
+
+            _productBacklogPage.CreateStoryButton.Click();
+
+            _createUserStoryPage.enterEmptyNameStoryDetails("e2eUserStory", "10");
+
+            var messageBox = MessageBoxUtil.GetWarningMessageBox(MainWindow);
+            Assert.NotNull(messageBox);
+
+            MessageBoxUtil.ClickOKButton(messageBox);
+        }
+
+        [Test]
+        public void ShouldFailCreatingAUserStoryWithEmptyMarketValue()
+        {
+            LoginPage.Login("user2@e2e.com", "Aut0mation");
+            Assert.IsTrue(_userDashboardPage.IsCurrentPage());
+
+            var projectListItem = (WPFListItem)_userDashboardPage.ProjectListBox.Items.Find(item => "e2eProjectName1".Equals(item.Text));
+            projectListItem.Click();
+
+            Assert.IsTrue(_projectDashboardPage.IsCurrentPage());
+
+            Assert.AreEqual("e2eForename2 e2eSurname2", _projectDashboardPage.ProjectManagerNameTextBlock.Text);
+
+            _projectDashboardPage.ProductBacklogButton.Click();
+
+            Assert.IsTrue(_productBacklogPage.IsCurrentPage());
+
+            _productBacklogPage.CreateStoryButton.Click();
+
+            _createUserStoryPage.enterEmptyDescriptionStoryDetails("e2eUserStory", "e2eDesciption");
+
+            var messageBox = MessageBoxUtil.GetWarningMessageBox(MainWindow);
+            Assert.NotNull(messageBox);
+
+            MessageBoxUtil.ClickOKButton(messageBox);
+        }
+
+        [Test]
+        public void ShouldFailCreatingAUserStoryWithTextMarketValue()
+        {
+            LoginPage.Login("user2@e2e.com", "Aut0mation");
+            Assert.IsTrue(_userDashboardPage.IsCurrentPage());
+
+            var projectListItem = (WPFListItem)_userDashboardPage.ProjectListBox.Items.Find(item => "e2eProjectName1".Equals(item.Text));
+            projectListItem.Click();
+
+            Assert.IsTrue(_projectDashboardPage.IsCurrentPage());
+
+            Assert.AreEqual("e2eForename2 e2eSurname2", _projectDashboardPage.ProjectManagerNameTextBlock.Text);
+
+            _projectDashboardPage.ProductBacklogButton.Click();
+
+            Assert.IsTrue(_productBacklogPage.IsCurrentPage());
+
+            _productBacklogPage.CreateStoryButton.Click();
+
+            _createUserStoryPage.enterEmptyNameStoryDetails("Ten", "e2eDescription");
+
+            var messageBox = MessageBoxUtil.GetWarningMessageBox(MainWindow);
+            Assert.NotNull(messageBox);
+
+
+            MessageBoxUtil.ClickOKButton(messageBox);
+        }
+    }
+}
