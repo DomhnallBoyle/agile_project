20999011374b290eb0dd1fe2d9c673ae98fb860e
Darren McGarry <darren.mcgarry@hotmail.com>
2017-11-30 21:36:35 +0000

    Darren & Kevin: Story 11c: Initial changes to models

---
+++ CSC3045-CS2-Frontend/CSC3045-CS2/Models/TaskEstimation.cs
@@ -0,0 +1,23 @@
+﻿using RestSharp.Deserializers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSC3045_CS2.Models
+{
+    public class TaskEstimation
+    {
+        [DeserializeAs(Name = "taskId")]
+        public long TaskId { get; set; }
+
+        [DeserializeAs(Name = "date")]
+        public DateTime Date { get; set; }
+
+        [DeserializeAs(Name = "estimate")]
+        public int Estimate { get; set; }
+
+        public TaskEstimation() { }
+    }
+}
