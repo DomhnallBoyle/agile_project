3cfade49e25d22ee2cc4477bab481bd6e3399253
Domhnall Boyle <=>
2017-12-01 04:44:19 +0000

    Domhnall: Service and Controller tests

--- CSC3045-CS2-Backend/src/test/java/uk/ac/qub/csc3045/api/controllers/UserControllerTest.java
+++ CSC3045-CS2-Backend/src/test/java/uk/ac/qub/csc3045/api/controllers/UserControllerTest.java
@@ -5,21 +5,40 @@ import org.junit.Test;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import uk.ac.qub.csc3045.api.controller.UserController;
+import uk.ac.qub.csc3045.api.model.Roles;
 import uk.ac.qub.csc3045.api.model.User;
 import uk.ac.qub.csc3045.api.service.UserService;
 
 import static org.junit.Assert.assertEquals;
 import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class UserControllerTest {
 
     private UserController userController;
     private UserService userService;
+    private User user;
+    private long userId;
+    private List<Skill> skills;
 
     @Before
     public void setUp() {
         userService = mock(UserService.class);
         userController = new UserController(userService);
+        userId = 1l;
+        skills = setUpSkills();
+        user = new User("Richard", "Hendrix", "r.hendrix@valley.com", new Roles(true, true, false), skills);
+    }
+    
+    private List<Skill> setUpSkills() {
+    	List<Skill> skills = new ArrayList<Skill>();
+    	for (int i=0; i<5; i++) {
+    		skills.add(new Skill("skill " + i, userId));
+    	}
+    	return skills;
     }
 
     @Test
@@ -29,5 +48,25 @@ public class UserControllerTest {
         ResponseEntity response = userController.search(searchCriteria);
 
         assertEquals(HttpStatus.OK, response.getStatusCode());
+    }
+    
+    @Test
+    public void getUserSkillsShouldReturn200OnSuccess() {
+    	when(userService.getUserSkills(userId).thenReturn(skills));
+    	
+    	ResponseEntity response = userController.getUserSkills(userId);
+    	
+    	assertEquals(HttpStatus.OK, response.getStatusCode());
+    	assertEquals(response.getBody(), skills);
+    }
+    
+    @Test
+    public void updateUserSkillsShouldReturn200OnSuccess() {
+    	when(userService.updateUserSkills(userId).thenReturn(skills));
+    	
+    	ResponseEntity response = userController.updateUserSkills(userId, user);
+    	
+    	assertEquals(HttpStatus.OK, response.getStatusCode());
+    	assertEquals(response.getBody(), skills);
     }
 }
--- CSC3045-CS2-Backend/src/test/java/uk/ac/qub/csc3045/api/service/UserServiceTest.java
+++ CSC3045-CS2-Backend/src/test/java/uk/ac/qub/csc3045/api/service/UserServiceTest.java
@@ -2,7 +2,9 @@ package uk.ac.qub.csc3045.api.service;
 
 import org.junit.Before;
 import org.junit.Test;
+import org.mockito.Matchers;
 
+import uk.ac.qub.csc3045.api.controllers.Skill;
 import uk.ac.qub.csc3045.api.exception.ResponseErrorException;
 import uk.ac.qub.csc3045.api.mapper.UserMapper;
 import uk.ac.qub.csc3045.api.model.Roles;
@@ -13,6 +15,10 @@ import static org.junit.Assert.assertTrue;
 import static org.junit.Assert.fail;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.doNothing;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.validation.constraints.AssertTrue;
 
@@ -20,14 +26,25 @@ public class UserServiceTest {
 
     private UserService userService;
     private UserMapper userMapper;
-
     private User user;
+    private List<Skill> skills;
+    private long userId;
 
     @Before
     public void setUp() {
+    	userId = 1l;
+    	skills = setUpSkills();
     	user = new User("Ragnar", "Lothbrok", "ragnar.lothbrok@valhalla.odin", new Roles(true, true, false));
     	userMapper = mock(UserMapper.class);
         userService = new UserService(userMapper);
+    }
+    
+    private List<Skill> setUpSkills() {
+    	List<Skill> skills = new ArrayList<Skill>();
+    	for (int i=0; i<5; i++) {
+    		skills.add(new Skill("skill " + i, userId));
+    	}
+    	return skills;
     }
 
     @Test
@@ -48,5 +65,25 @@ public class UserServiceTest {
     	catch(ResponseErrorException e) {
     		assertTrue(e.getMessage() == "User with the specified email does not exist");
     	}
+    }
+    
+    @Test
+    public void getUserSkillsShouldReturn200() {
+    	when(userMapper.addUserSkill(userId, Matchers.any(Skill.class)));
+    	
+    	List<Skill> returnedSkills = userService.getUserSkills(userId);
+    	
+    	assertEquals(skills, returnedSkills);
+    }
+    
+    @Test
+    public void updateUserSkillsShouldReturn200() {
+    	doNothing().when(userMapper.removeUserSkills(userId));
+    	doNothing().when(userMapper.addUserSkill(userId, Matchers.any(Skill.class)));
+    	when(userMapper.getUserSkills(userId)).thenReturn(skills);
+    	
+    	List<Skill> returnedSkills = userService.updateUserSkills(userId, user);
+    	
+    	assertEquals(skills, returnedSkills);
     }
 }
